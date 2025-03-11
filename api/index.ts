19d55b9a15ec69c51854b3548be671d224dd8dfd
rizkyathoriq19 <rizkyathoriq19@gmail.com>
2025-03-11 14:46:53 +0700

    feat: fix vercel

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,6 @@
 import { handle } from "@hono/node-server/vercel";
 
-// eslint-disable-next-line ts/ban-ts-comment
-// @ts-expect-error
-// eslint-disable-next-line antfu/no-import-dist
+// @ts-ignore
 import app from "../dist/src/index.js"
 
 export default handle(app)
